dba2c8164ba8215d064f78bddb9ee7c94cd69674
Mo Khosh <mskhoshnazar@gmail.com>
2024-10-18 16:52:02 +0330

    add empty livewire component with forms

---
+++ app/Livewire/UploadBug.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire;
+
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Livewire\Component;
+
+/**
+ * @property Form $form
+ */
+class UploadBug extends Component implements HasForms
+{
+    use InteractsWithForms;
+
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ])
+            ->statePath('data');
+    }
+
+    public function create(): void
+    {
+        $this->form->getState();
+    }
+}
