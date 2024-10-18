7231059c15e365993153bdc636d9efefce7e580a
Mo Khosh <mskhoshnazar@gmail.com>
2024-10-18 18:02:53 +0330

    fix dusk not running

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,12 +1,9 @@
 <?php
 
-pest()->extend(Tests\DuskTestCase::class)
-//  ->use(Illuminate\Foundation\Testing\DatabaseMigrations::class)
+pest()
+    ->extend(Tests\DuskTestCase::class)
     ->in('Browser');
 
-pest()->extend(Tests\DuskTestCase::class)
-//  ->use(Illuminate\Foundation\Testing\DatabaseMigrations::class)
-    ->in('Browser');
 
 /*
 |--------------------------------------------------------------------------
