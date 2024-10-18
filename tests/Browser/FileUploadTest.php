aabdb7df7cc5d0a87fecc847a3bc469a5f74487f
Mo Khosh <mskhoshnazar@gmail.com>
2024-10-18 18:09:52 +0330

    add failing test

--- tests/Browser/FileUploadTest.php
+++ tests/Browser/FileUploadTest.php
@@ -5,6 +5,11 @@ use Laravel\Dusk\Browser;
 test('example', function () {
     $this->browse(function (Browser $browser) {
         $browser->visit('/')
-                ->assertSee('Laravel');
+                ->assertSee('File')
+                ->pause(1000);
+
+        $log = $browser->driver->manage()->getLog('browser');
+
+        $this->assertEmpty($log);
     });
 });
